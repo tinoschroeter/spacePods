fc36d48255254c6a5885454053a211d105e474da
Tino Schroeter <tino.schroter@gmail.com>
2022-04-23 14:36:42 +0200

    add: first working backend

--- backend/index.js
+++ backend/index.js
@@ -6,29 +6,44 @@ const cors = require("cors");
 app.use(morgan("combined"));
 app.use(cors());
 
-let liste = [
-  { name: "web-host-547dcu815-2d1ha" },
-  { name: "web-host-677dcb845-2d2hb" },
-  { name: "web-host-457dcg865-2d3hc" },
-  { name: "web-host-657dcd845-2d4hd" },
-  { name: "web-host-637dcs885-2d5he" },
-  { name: "web-host-357dct845-2d6hf" },
-  { name: "web-host-157dca485-2d7hg" },
-];
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+const k8s = require("@kubernetes/client-node");
+const fetch = require("node-fetch");
+
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
+
+const opts = {};
+kc.applyToRequest(opts);
 
 app.get("/api/pods", (req, res) => {
-  res.json(liste);
+  fetch(
+    `${kc.getCurrentCluster().server}/api/v1/namespaces/spacepods/pods`,
+    opts
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      res.json(data);
+    });
 });
 
 app.delete("/api/pods/:id", (req, res) => {
   const { id } = req.params;
-
+  console.log("id: ", id);
+  console.log("req.params: ", req.params);
   if (id) {
-    liste = liste.filter((item) => item.name !== id);
-    console.log("delete ", id);
-    return res.json({ delete: id });
+    const options = { ...opts, method: "DELETE" };
+    fetch(
+      `${kc.getCurrentCluster().server}/api/v1/namespaces/spacepods/pods/${id}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        return res.json(data);
+      })
+      .catch((error) => res.status(500).json({ error }));
   }
-
   res.status(400).json({ error: "user error" });
 });
 
