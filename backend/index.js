a603f7ede5097a10d380356a3edc7dd911a7c781
Tino Schroter <tino.schroeter@gmail.com>
2022-06-16 21:08:57 +0200

    add: prometheus metrics

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const promMid = require("express-prometheus-middleware");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const app = express();
@@ -10,6 +11,16 @@ const apiLimiter = rateLimit({
   standardHeaders: true,
   legacyHeaders: false,
 });
+
+app.use(
+  promMid({
+    metricsPath: "/metrics",
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+  })
+);
 
 app.use("/api", apiLimiter);
 app.use(morgan("combined"));
